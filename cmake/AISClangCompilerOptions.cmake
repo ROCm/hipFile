1f8e523a11c0f62c18d4aa3a6bba49635479ab88
Dana Robinson <Dana.Robinson@amd.com>
2025-11-23 12:23:35 -0700

    Fix CMake lint issues

--- cmake/AISClangCompilerOptions.cmake
+++ cmake/AISClangCompilerOptions.cmake
@@ -231,7 +231,7 @@ function(get_ais_clang_warning_flags outvar compiler_version)
 
     # Only use _FORTIFY_SOURCE if the optimization level is -O2, -O3, or -Os
     string(JOIN " " MYCXXFLAGS ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}})
-    if (MYCXXFLAGS MATCHES "-O[2-3s]")
+    if(MYCXXFLAGS MATCHES "-O[2-3s]")
         set(flags
             -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
             ${flags}
--- cmake/AISGNUCompilerOptions.cmake
+++ cmake/AISGNUCompilerOptions.cmake
@@ -93,7 +93,7 @@ function(get_ais_gnu_warning_flags outvar compiler_version)
     # Only use _FORTIFY_SOURCE if the optimization level is -O2, -O3, or -Os
     if(compiler_version VERSION_GREATER_EQUAL 12)
         string(JOIN " " MYCXXFLAGS ${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}})
-        if (MYCXXFLAGS MATCHES "-O[2-3s]")
+        if(MYCXXFLAGS MATCHES "-O[2-3s]")
             set(flags
                 -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=3
                 ${flags}
