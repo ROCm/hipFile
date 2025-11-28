7d93a7b2cca0374ed27aceb44f4397bb4e56010f
Kurt McMillan <Kurt.McMillan@amd.com>
2025-11-28 22:03:02 +0000

    review: Address PR comments
    - The github job now manages the lifetime of the IO test directory which
      ensures it is always cleaned up (even when the job is cancelled)
    - IO tests still manage their own temporary directory within the IO test
      directory to ensure that disk space is released at the end of the test.

--- util/ci-aiscp-test.sh
+++ util/ci-aiscp-test.sh
@@ -3,24 +3,25 @@
 #
 # SPDX-License-Identifier: MIT
 
-# Used by CI to test hipFile IO using aiscp
+# Used by CI to test hipFile IO using aiscp.
+#
+# NOTE: CI must ensure this script is executed within a directory on an AIS
+# supported filesystem backed by an AIS supported device.
 
 # Usage:
-# ci-aiscp-test.sh WORKDIR AISCP
-#   WORKDIR: A directory on an AIS supported filesystem backed by an AIS supported device
+# ci-aiscp-test.sh AISCP
 #   AISCP: Path to the aiscp binary
 
 set -e
 
 trap 'if [ -e "$TMPDIR" ]; then rm -fr "$TMPDIR"; fi' EXIT
 
-WORKDIR=$1
-AISCP=$2
+AISCP=$1
 
-echo "Test directory: $WORKDIR"
+echo "pwd: $(pwd)"
 echo "aiscp: $AISCP"
 
-TMPDIR=$(mktemp --directory --tmpdir="$WORKDIR")
+TMPDIR=$(mktemp --directory --tmpdir=.)
 SRC="$TMPDIR/rand.dat"
 DST="$TMPDIR/rand.dat.cp"
 echo "Source: $SRC"
@@ -33,8 +34,8 @@ head --bytes 1G /dev/urandom > "$SRC"
 SRC_MD5=$(md5sum "$SRC" | awk '{ print $1 }')
 DST_MD5=$(md5sum "$DST" | awk '{ print $1 }')
 
-echo "Source file md5:      $SRC_MD5"
-echo "Destination file md5: $DST_MD5"
+echo "$SRC_MD5 $SRC"
+echo "$DST_MD5 $DST"
 
 if [ "$SRC_MD5" != "$DST_MD5" ]; then
     echo "Files differ! Test failed. MD5 checksums do not match."
