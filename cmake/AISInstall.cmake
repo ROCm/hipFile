0398cb98063e920dae12aa820b48c31d2bfce246
Riley Dixon <riley.dixon@amd.com>
2025-12-08 15:40:14 -0700

    CMake: Add pkg dependencies for hipFile
    Right now both runtime and development dependencies are added to
    all builds. Once we have Debug/Release builds configured we can
    split up when these dependencies are added.

--- cmake/AISInstall.cmake
+++ cmake/AISInstall.cmake
@@ -18,8 +18,35 @@ rocm_install(
     DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
-# rocm_package_add_dependencies() line should go here
-# when we have dependencies
+# When we have RELEASE/DEV builds set up, we can split
+# where these dependencies are added.
+# For now, just add both runtime & dev deps.
+
+# AMD Runtime Dependencies
+if(CMAKE_HIP_PLATFORM STREQUAL "amd")
+    rocm_package_add_dependencies(DEPENDS hip-runtime-amd) # Need minimum version
+    rocm_package_add_dependencies(DEPENDS libmount)
+endif()
+
+# AMD Development Dependencies
+if(CMAKE_HIP_PLATFORM STREQUAL "amd")
+    rocm_package_add_deb_dependencies(DEPENDS hip-dev) # Need minimum version
+    rocm_package_add_rpm_dependencies(DEPENDS hip-devel)
+
+    rocm_package_add_deb_dependencies(DEPENDS libmount-dev)
+    rocm_package_add_rpm_dependencies(DEPENDS libmount-devel)
+endif()
+
+# Nvidia Runtime Dependencies
+if(CMAKE_HIP_PLATFORM STREQUAL "nvidida")
+    rocm_package_add_dependencies(DEPENDS libcufile)
+endif()
+
+# Nvidia Development Dependencies
+if(CMAKE_HIP_PLATFORM STREQUAL "nvidia")
+    rocm_package_add_deb_dependencies(DEPENDS libcufile-dev)
+    rocm_package_add_rpm_dependencies(DEPENDS libcufile-devel)
+endif()
 
 # Export the targets
 set(target_list ${target_list} roc::hipfile_shared roc::hipfile_static)
