e505a011d5ecd152f9563834338576f1ac7579c7
Kurt McMillan <Kurt.McMillan@amd.com>
2025-12-08 20:40:54 +0000

    hipfile: Introduce class to manage file descriptor lifetimes

---
+++ test/amd_detail/file-descriptor.cpp
@@ -0,0 +1,93 @@
+/* Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "file-descriptor.h"
+#include "msys.h"
+
+#include <gtest/gtest.h>
+
+using namespace hipFile;
+using namespace testing;
+
+HIPFILE_WARN_NO_GLOBAL_CTOR_OFF
+
+struct HipFileFileDescriptor : public Test {
+    StrictMock<MSys> msys;
+};
+
+TEST_F(HipFileFileDescriptor, ManagedFileDescriptorClosesOnDestruction)
+{
+    auto fd{FileDescriptor::make_managed(42)};
+    EXPECT_CALL(msys, close(42));
+}
+
+TEST_F(HipFileFileDescriptor, ManagedFileDescriptorDoesNotCloseOnDestructionIfFdIsNegativeOne)
+{
+    auto fd{FileDescriptor::make_managed(-1)};
+}
+
+TEST_F(HipFileFileDescriptor, UnmanagedFileDescriptorDoesNotCloseOnDestruction)
+{
+    auto fd{FileDescriptor::make_unmanaged(42)};
+}
+
+TEST_F(HipFileFileDescriptor, ManagedFileDescriptorMoveCopyConstructor)
+{
+    auto fd_a{FileDescriptor::make_managed(42)};
+    auto fd_b{std::move(fd_a)};
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+    EXPECT_CALL(msys, close(42));
+}
+
+TEST_F(HipFileFileDescriptor, UnmanagedFileDescriptorMoveCopyConstructor)
+{
+    auto fd_a{FileDescriptor::make_unmanaged(42)};
+    auto fd_b{std::move(fd_a)};
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+}
+
+TEST_F(HipFileFileDescriptor, ManagedFileDescriptorToManagedFileDescriptorMoveAssignment)
+{
+    auto fd_a{FileDescriptor::make_managed(42)};
+    auto fd_b{FileDescriptor::make_managed(12)};
+    EXPECT_CALL(msys, close(12));
+    fd_b = std::move(fd_a);
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+    EXPECT_CALL(msys, close(42));
+}
+
+TEST_F(HipFileFileDescriptor, ManagedFileDescriptorToUnmanagedFileDescriptorMoveAssignment)
+{
+    auto fd_a{FileDescriptor::make_managed(42)};
+    auto fd_b{FileDescriptor::make_unmanaged(12)};
+    fd_b = std::move(fd_a);
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+    EXPECT_CALL(msys, close(42));
+}
+
+TEST_F(HipFileFileDescriptor, UnmanagedFileDescriptorToManagedFileDescriptorMoveAssignment)
+{
+    auto fd_a{FileDescriptor::make_unmanaged(42)};
+    auto fd_b{FileDescriptor::make_managed(12)};
+    EXPECT_CALL(msys, close(12));
+    fd_b = std::move(fd_a);
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+}
+
+TEST_F(HipFileFileDescriptor, UnmanagedFileDescriptorToUnmanagedFileDescriptorMoveAssignment)
+{
+    auto fd_a{FileDescriptor::make_unmanaged(42)};
+    auto fd_b{FileDescriptor::make_unmanaged(12)};
+    fd_b = std::move(fd_a);
+    ASSERT_EQ(fd_a.get(), -1);
+    ASSERT_EQ(fd_b.get(), 42);
+}
+
+HIPFILE_WARN_NO_GLOBAL_CTOR_ON
