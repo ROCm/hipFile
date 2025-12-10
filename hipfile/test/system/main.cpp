ad84ad898935369163b2006148de40c5f4da32f3
Jordan Patterson <jordan.patterson@amd.com>
2025-12-10 12:27:51 -0500

    hipFile: Add sleep to system tests during program termination.

--- hipfile/test/system/main.cpp
+++ hipfile/test/system/main.cpp
@@ -6,7 +6,9 @@
 #include "hipfile-warnings.h"
 #include "test-options.h"
 
+#include <chrono>
 #include <gtest/gtest.h>
+#include <thread>
 
 extern SystemTestOptions test_env;
 HIPFILE_WARN_NO_GLOBAL_CTOR_OFF
@@ -15,12 +17,21 @@ SystemTestOptions test_env;
 HIPFILE_WARN_NO_EXIT_DTOR_ON
 HIPFILE_WARN_NO_GLOBAL_CTOR_ON
 
+static void
+sleepOnExit()
+{
+    if (test_env.sleep_seconds > 0) {
+        std::this_thread::sleep_for(std::chrono::seconds(test_env.sleep_seconds));
+    }
+}
+
 int
 main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
     test_env.parseTestOptions(argc, argv);
+    std::atexit(sleepOnExit);
 
     return RUN_ALL_TESTS();
 }
