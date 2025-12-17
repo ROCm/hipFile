9ae5b6a9e2c9d54bde5e7592e1d41e3bc93b7b26
Kurt McMillan <Kurt.McMillan@amd.com>
2025-12-17 21:14:38 +0000

    hipfile: Remove exception type from sys.cpp:throwOn(...)

--- src/amd_detail/sys.cpp
+++ src/amd_detail/sys.cpp
@@ -14,12 +14,12 @@
 
 namespace hipFile {
 
-template <typename ExceptionType, typename L, typename R>
+template <typename L, typename R>
 static inline R
 throwOn(const L throw_value, R value)
 {
     if (throw_value == value) {
-        throw ExceptionType();
+        throw Sys::RuntimeError();
     }
     return value;
 }
@@ -27,56 +27,55 @@ throwOn(const L throw_value, R value)
 int
 Sys::open(const char *pathname, int flags) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::open(pathname, flags));
+    return throwOn(-1, ::open(pathname, flags));
 }
 
 int
 Sys::open(const char *pathname, int flags, mode_t mode) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::open(pathname, flags, mode));
+    return throwOn(-1, ::open(pathname, flags, mode));
 }
 
 void
 Sys::close(int fd) const
 {
-    throwOn<Sys::RuntimeError>(-1, ::close(fd));
+    throwOn(-1, ::close(fd));
 }
 
 void
 Sys::fdatasync(int fd) const
 {
-    throwOn<Sys::RuntimeError>(-1, ::fdatasync(fd));
+    throwOn(-1, ::fdatasync(fd));
 }
 
 void *
 Sys::mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset) const
 {
-    return throwOn<Sys::RuntimeError>(reinterpret_cast<void *>(-1),
-                                      ::mmap(addr, length, prot, flags, fd, offset));
+    return throwOn(reinterpret_cast<void *>(-1), ::mmap(addr, length, prot, flags, fd, offset));
 }
 
 void
 Sys::munmap(void *addr, size_t length) const
 {
-    throwOn<Sys::RuntimeError>(-1, ::munmap(addr, length));
+    throwOn(-1, ::munmap(addr, length));
 }
 
 ssize_t
 Sys::pread(int fd, void *buf, size_t count, off_t offset) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::pread(fd, buf, count, offset));
+    return throwOn(-1, ::pread(fd, buf, count, offset));
 }
 
 ssize_t
 Sys::pwrite(int fd, void *buf, size_t count, off_t offset) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::pwrite(fd, buf, count, offset));
+    return throwOn(-1, ::pwrite(fd, buf, count, offset));
 }
 
 ssize_t
 Sys::readlink(const char *pathname, char *buf, size_t bufsize) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::readlink(pathname, buf, bufsize));
+    return throwOn(-1, ::readlink(pathname, buf, bufsize));
 }
 
 void
@@ -90,7 +89,7 @@ Sys::fstat(int fd) const
 {
     struct stat statbuf;
 
-    throwOn<Sys::RuntimeError>(-1, ::fstat(fd, &statbuf));
+    throwOn(-1, ::fstat(fd, &statbuf));
 
     return statbuf;
 }
@@ -98,14 +97,14 @@ Sys::fstat(int fd) const
 int
 Sys::fcntl(int fd, int op, uintptr_t arg) const
 {
-    return throwOn<Sys::RuntimeError>(-1, ::fcntl(fd, op, arg));
+    return throwOn(-1, ::fcntl(fd, op, arg));
 }
 
 struct statx
 Sys::statx(int dirfd, const char *pathname, int flags, unsigned int mask) const
 {
     struct statx statxbuf;
-    throwOn<Sys::RuntimeError>(-1, ::statx(dirfd, pathname, flags, mask, &statxbuf));
+    throwOn(-1, ::statx(dirfd, pathname, flags, mask, &statxbuf));
     return statxbuf;
 }
 
