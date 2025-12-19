cf76d7276b7226b6769808819df55117188c79f2
Kurt McMillan <Kurt.McMillan@amd.com>
2025-12-19 16:51:08 +0000

    hipfile: Wrap USDT(...) with USDT_HIPFILE(...)
    Ensure that all hipfile probes have the same group identifier.

---
+++ src/amd_detail/trace.h
@@ -0,0 +1,10 @@
+/* Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include "usdt.h"
+
+#define USDT_HIPFILE(...) USDT(hipfile, __VA_ARGS__)
