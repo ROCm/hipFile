95faa4f09b4f2595e7a0445a3b9bbc038cf0e60d
Dana Robinson <43805+derobins@users.noreply.github.com>
2025-12-31 08:51:23 -0800

    CMake: Only build static OR shared (#146)
    * Make `BUILD_SHARED_LIBS` an option
    * Switch from building static and shared to just one (based on
    `BUILD_SHARED_LIBS` value)
    * Quiet RPM warnings about asking for relocatable packages with fixed
    paths. We now only build with fixed paths, like the rest of ROCm.
    
    This brings the install behaviour in line with the rest of ROCm

--- cmake/AISAddLibraries.cmake
+++ cmake/AISAddLibraries.cmake
@@ -5,7 +5,9 @@
 include(AISCompilerOptions)
 include(CheckLinkerFlag)
 
-# Add static and shared libraries using AIS build conventions
+# Add a library using AIS build conventions. Shared vs. static
+# is decided by the BUILD_SHARED_LIBS option and only one can
+# be built at a time (like the rest of ROCm).
 #
 # Parameters:
 #   NAME     <name>                             The name of the libraries to create
@@ -22,23 +24,14 @@ function(ais_add_libraries)
     set(multiValueArgs SRCS SYSINCLS LIBS)
     cmake_parse_arguments(arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Convenience names
-    set(object_name "${arg_NAME}_object")
-    set(static_name "${arg_NAME}_static")
-    set(shared_name "${arg_NAME}_shared")
-
-    # Add the object, shared, and static library targets
-    #
-    # The static and shared libraries are both built from the
-    # same compiled objects (with -fPIC, etc.)
-    add_library(${object_name} OBJECT ${arg_SRCS})
-    add_library(${static_name} STATIC $<TARGET_OBJECTS:${object_name}>)
-    add_library(${shared_name} SHARED $<TARGET_OBJECTS:${object_name}>)
+    # Add the library target
+    add_library(${arg_NAME} ${arg_SRCS})
 
     # Add version numbers
-    set_target_properties(${static_name} PROPERTIES VERSION ${AIS_LIBRARY_VERSION})
-    set_target_properties(${shared_name} PROPERTIES VERSION ${AIS_LIBRARY_VERSION})
-    set_target_properties(${shared_name} PROPERTIES SOVERSION ${AIS_LIBRARY_SOVERSION})
+    set_target_properties(${arg_NAME} PROPERTIES VERSION ${AIS_LIBRARY_VERSION})
+    if(BUILD_SHARED_LIBS)
+        set_target_properties(${arg_NAME} PROPERTIES SOVERSION ${AIS_LIBRARY_SOVERSION})
+    endif()
 
     # Add dependencies on external libraries
     foreach(lib IN LISTS arg_LIBS)
@@ -46,59 +39,55 @@ function(ais_add_libraries)
         if(NOT LIBRARY_PATH)
             message(FATAL_ERROR "lib${lib} not found")
         endif()
-        target_link_libraries(${static_name} PRIVATE ${LIBRARY_PATH})
-        target_link_libraries(${shared_name} PRIVATE ${LIBRARY_PATH})
+        target_link_libraries(${arg_NAME} PRIVATE ${LIBRARY_PATH})
     endforeach()
 
-    foreach(target "${object_name}" "${static_name}" "${shared_name}")
+    if(BUILD_TESTING)
+        target_compile_definitions(${arg_NAME} PRIVATE AIS_TESTING)
+    endif()
 
-        if(BUILD_TESTING)
-            target_compile_definitions(${target} PRIVATE AIS_TESTING)
-        endif()
+    if(arg_DETAIL STREQUAL "nvidia")
+        target_compile_definitions(${arg_NAME} PRIVATE __HIP_PLATFORM_NVIDIA__)
+    else()
+        target_compile_definitions(${arg_NAME} PRIVATE __HIP_PLATFORM_AMD__)
+    endif()
 
-        if(arg_DETAIL STREQUAL "nvidia")
-            target_compile_definitions(${target} PRIVATE __HIP_PLATFORM_NVIDIA__)
-        else()
-            target_compile_definitions(${target} PRIVATE __HIP_PLATFORM_AMD__)
-        endif()
+    # Set the library name, minus the .a, .so, or whatever
+    set_target_properties(${arg_NAME} PROPERTIES OUTPUT_NAME ${arg_NAME})
 
-        # Set the library name, minus the .a, .so, or whatever
-        set_target_properties(${target} PROPERTIES OUTPUT_NAME ${arg_NAME})
+    # Set the public include paths (these are installed libraries)
+    target_include_directories(${arg_NAME}
+        PUBLIC
+        $<BUILD_INTERFACE:${arg_LIBINCL}>
+        $<INSTALL_INTERFACE:include>
+    )
 
-        # Set the public include paths (these are installed libraries)
-        target_include_directories(${target}
-            PUBLIC
-            $<BUILD_INTERFACE:${arg_LIBINCL}>
-            $<INSTALL_INTERFACE:include>
+    # Add other include paths
+    foreach(incl IN LISTS arg_SYSINCLS)
+        target_include_directories(${arg_NAME} SYSTEM PRIVATE ${incl})
+    endforeach()
+
+    # Add libraries and headers for NVIDIA targets
+    if(arg_DETAIL STREQUAL "nvidia")
+        target_include_directories(${arg_NAME}
+            SYSTEM
+            PRIVATE
+            ${HIP_INCLUDE_DIRS}
+            ${CUDAToolkit_INCLUDE_DIRS}
         )
+        target_link_libraries(${arg_NAME} PRIVATE ${CUFILE_LIBRARY})
+    endif()
+    target_link_libraries(${arg_NAME} PRIVATE hip::host)
 
-        # Add other include paths
-        foreach(incl IN LISTS arg_SYSINCLS)
-            target_include_directories(${target} SYSTEM PRIVATE ${incl})
-        endforeach()
+    # Add link options (mainly for hardening)
+    check_linker_flag(CXX "-Wl,-z,noexecstack" LINKER_SUPPORTS_NOEXECSTACK)
+    if(LINKER_SUPPORTS_NOEXECSTACK)
+        target_link_options(${arg_NAME} PRIVATE "-Wl,-z,noexecstack")
+    endif()
 
-        # Add libraries and headers for NVIDIA targets
-        if(arg_DETAIL STREQUAL "nvidia")
-            target_include_directories(${target}
-                SYSTEM
-                PRIVATE
-                ${HIP_INCLUDE_DIRS}
-                ${CUDAToolkit_INCLUDE_DIRS}
-            )
-            target_link_libraries(${target} PRIVATE ${CUFILE_LIBRARY})
-        endif()
-        target_link_libraries(${target} PRIVATE hip::host)
+    # Add the common include path
+    target_include_directories(${arg_NAME} PRIVATE "${CMAKE_SOURCE_DIR}/shared")
 
-        # Add link options (mainly for hardening)
-        check_linker_flag(CXX "-Wl,-z,noexecstack" LINKER_SUPPORTS_NOEXECSTACK)
-        if(LINKER_SUPPORTS_NOEXECSTACK)
-            target_link_options(${target} PRIVATE "-Wl,-z,noexecstack")
-        endif()
-
-        # Add the common include path
-        target_include_directories(${target} PRIVATE "${CMAKE_SOURCE_DIR}/shared")
-
-        # Set compiler flags
-        ais_set_compiler_flags(${target})
-    endforeach()
+    # Set compiler flags
+    ais_set_compiler_flags(${arg_NAME})
 endfunction()
