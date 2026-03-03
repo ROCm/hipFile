14f2dce45d86c8c1e23520735b0d36135fdc813d
zbyrne <zbyrne@amd.com>
2026-03-03 16:06:07 -0500

    Add HIPFILE_STATS_LEVEL environment variable.

--- test/amd_detail/environment.cpp
+++ test/amd_detail/environment.cpp
@@ -63,4 +63,32 @@ TEST(HipFileEnvironment, GetBoolReturnsOptionalFalseIfFalse)
     ASSERT_EQ(hipFile::Environment::get<bool>(""), make_optional<>(false));
 }
 
+TEST(HipFileEnvironment, GetUnsignedIntReturnsNulloptIfNotSet)
+{
+    StrictMock<MSys> msys;
+
+    EXPECT_CALL(msys, getenv).WillOnce(Return(nullptr));
+    ASSERT_EQ(hipFile::Environment::get<unsigned int>(""), nullopt);
+}
+
+TEST(HipFileEnvironment, GetUnsignedIntReturnsNulloptIfValueIsInvalid)
+{
+    StrictMock<MSys> msys;
+
+    EXPECT_CALL(msys, getenv).WillOnce(Return(const_cast<char *>("blah")));
+    ASSERT_EQ(hipFile::Environment::get<unsigned int>(""), nullopt);
+    EXPECT_CALL(msys, getenv).WillOnce(Return(const_cast<char *>("")));
+    ASSERT_EQ(hipFile::Environment::get<unsigned int>(""), nullopt);
+    EXPECT_CALL(msys, getenv).WillOnce(Return(const_cast<char *>("1abc")));
+    ASSERT_EQ(hipFile::Environment::get<unsigned int>(""), nullopt);
+}
+
+TEST(HipFileEnvironment, GetUnsignedIntReturnsIntIfValueIsInt)
+{
+    StrictMock<MSys> msys;
+
+    EXPECT_CALL(msys, getenv).WillOnce(Return(const_cast<char *>("0")));
+    ASSERT_EQ(hipFile::Environment::get<unsigned int>(""), make_optional<>(0));
+}
+
 HIPFILE_WARN_NO_GLOBAL_CTOR_ON
