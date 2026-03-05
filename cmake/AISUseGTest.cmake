36eaea79181866d9529d5f55bdb1510723e2589f
Dana Robinson <Dana.Robinson@amd.com>
2026-03-05 10:48:03 -0700

    Expand GTest build to other sanitizers

--- cmake/AISUseGTest.cmake
+++ cmake/AISUseGTest.cmake
@@ -9,7 +9,7 @@ include(FetchContent)
 # with the sanitizers, we HAVE to build GoogleTest from source.
 #
 # lint_cmake: -readability/wonkycase
-if(AIS_USE_SANITIZERS)
+if(AIS_USE_SANITIZERS OR AIS_USE_INTEGER_SANITIZER OR AIS_USE_THREAD_SANITIZER)
     FetchContent_Declare(
       googletest
       URL https://github.com/google/googletest/releases/download/v1.17.0/googletest-1.17.0.tar.gz
