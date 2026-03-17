f275f90423527da12a84c88aab428a7345c36f5c
Dana Robinson <Dana.Robinson@amd.com>
2026-03-17 15:32:40 -0600

    Fixes that make Sphinx "work"

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ version_number = "0.2.0"
 left_nav_title = f"hipFile {version_number} documentation"
 
 # for PDF output on Read the Docs
-project = "hipFile"
+project = "rocSHMEM"
 author = "Advanced Micro Devices, Inc."
 copyright = "Copyright (c) Advanced Micro Devices, Inc. All rights reserved."
 version = version_number
@@ -21,3 +21,7 @@ external_toc_path = "./sphinx/_toc.yml"
 docs_core = ROCmDocs(left_nav_title)
 docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
 docs_core.setup()
+
+external_projects_current_project = "rocshmem"
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
