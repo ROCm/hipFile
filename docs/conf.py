cfeebcb9ff3900b0b0872d527699e98dee36e97b
Dana Robinson <Dana.Robinson@amd.com>
2026-03-17 14:46:47 -0600

    Add conf.py

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from rocm_docs import ROCmDocs
+
+version_number = "0.2.0"
+left_nav_title = f"hipFile {version_number} documentation"
+
+# for PDF output on Read the Docs
+project = "hipFile"
+author = "Advanced Micro Devices, Inc."
+copyright = "Copyright (c) Advanced Micro Devices, Inc. All rights reserved."
+version = version_number
+release = version_number
+
+external_toc_path = "./sphinx/_toc.yml"
+
+docs_core = ROCmDocs(left_nav_title)
+docs_core.run_doxygen(doxygen_root="doxygen", doxygen_path="doxygen/xml")
+docs_core.setup()
